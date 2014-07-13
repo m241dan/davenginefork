ec473a9fa3e1bc66b5bcd7136d392295721fb16e
Davenge <korisd@gmail.com>
2014-07-13 09:58:41 -0400

    a lot of specifications utility commands have been added

--- src/specifications.c
+++ src/specifications.c
@@ -7,3 +7,112 @@ const char *const spec_table[] = {
    '\0' /* gandalf */
 };
 
+
+SPECIFICATION *init_specification( void )
+{
+   SPECIFICATION *spec;
+
+   CREATE( spec, SPECIFICATION, 1 );
+   if( clear_specification( spec ) != RET_SUCCESS )
+   {
+      bug( "%s: could not clear specification.", __FUNCTION__ );
+      return NULL;
+   }
+   return spec;
+}
+
+int clear_specification( SPECIFICATION *spec )
+{
+   int ret = RET_SUCCESS;
+
+   spec->type = -1;
+   spec->value = -1;
+   spec->owner = strdup( "null" );
+
+   return ret;
+}
+
+int free_specification( SPECIFICATION *spec )
+{
+   int ret = RET_SUCCESS;
+
+   FREE( spec->owner );
+   FREE( spec);
+
+   return ret;
+}
+
+int new_specification( SPECIFICATION *spec )
+{
+   int ret = RET_SUCCESS;
+
+   if( spec->type <= -1 || !strcmp( spec->owner, "null" ) )
+   {
+      bug( "%s: trying to add invalid spec to database.", __FUNCTION__ );
+      return RET_FAILED_OTHER;
+   }
+
+   if( !quick_query( "INSERT INTO live_specs VALUES( %d, %d, '%s' );", spec->type, spec->value, spec->owner ) )
+      return RET_FAILED_OTHER;
+
+   return ret;
+}
+
+int add_spec_to_framework( SPECIFICATION *spec, ENTITY_FRAMEWORK *frame )
+{
+   FREE( spec->owner );
+   mud_printf( spec->owner, "f%d", frame->tag->id );
+
+   AttachToList( spec, frame->specifications );
+
+   return RET_SUCCESS;
+}
+
+int load_specifications_to_list( LLIST *spec_list, const char *owner )
+{
+   SPECIFICATION *spec;
+   MYSQL_RES *result;
+   MYSQL_ROW row;
+
+   int ret = RET_SUCCESS;
+
+   if( !spec_list )
+   {
+      BAD_POINTER( "spec_list" );
+      return ret;
+   }
+   if( !owner || owner[0] == '\0' )
+   {
+      BAD_POINTER( "spec_list" );
+      return ret;
+   }
+
+   if( !quick_query( "SELECT * FROM live_specs WHERE owner='%s';", owner ) )
+      return RET_FAILED_OTHER;
+
+   if( ( result = mysql_store_result( sql_handle ) ) == NULL )
+      return RET_FAILED_OTHER;
+   if( mysql_num_rows( result ) == 0 )
+      return RET_DB_NO_ENTRY;
+
+   while( ( row = mysql_fetch_row( result ) ) != NULL )
+   {
+      spec = init_specification();
+      if ( db_load_spec( spec, &row ) != RET_SUCCESS )
+      {
+         free_specification( spec );
+         continue;
+      }
+      AttachToList( spec, spec_list );
+   }
+   mysql_free_result( result );
+   return RET_SUCCESS;
+}
+
+int db_load_spec( SPECIFICATION *spec, MYSQL_ROW *row )
+{
+   spec->type = atoi( (*row)[0] );
+   spec->value = atoi( (*row)[1] );
+   spec->owner = strdup( (*row)[2] );
+   return RET_SUCCESS;
+}
--- src/specifications.h
+++ src/specifications.h
@@ -6,5 +6,14 @@ struct typSpec
 {
    short type;
    int value;
-   const char *owner;
+   char *owner;
 };
+
+SPECIFICATION *init_specification( void );
+int clear_specification( SPECIFICATION *spec );
+int free_specification( SPECIFICATION *spec );
+
+int new_specification( SPECIFICATION *spec );
+int add_spec_to_framework( SPECIFICATION *spec, ENTITY_FRAMEWORK *frame );
+int load_specifications_to_list( LLIST *spec_list, const char *owner );
+int db_load_spec( SPECIFICATION *spec, MYSQL_ROW *row );
