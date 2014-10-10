7cc2c1de9376fad568085861484b0d8a63abe052
Davenge <korisd@gmail.com>
2014-10-10 11:22:03 -0400

    converted to strings.h and added the start to my string struct

--- src/mud.h
+++ src/mud.h
@@ -434,42 +434,6 @@ int directory_file_count_regex( DIR *directory, const char *regex_string );
  */
 int change_socket_state( D_SOCKET *dsock, int state );
 
-/* 
- * strings.c
- */
-char   *one_arg               ( char *fStr, char *bStr );
-char   *one_arg_delim         ( char *fStr, char *bStr, char delim );
-char   *strdup                ( const char *s );
-int     strcasecmp            ( const char *s1, const char *s2 );
-bool    is_prefix             ( const char *aStr, const char *bStr );
-char   *capitalize            ( char *txt );
-char   *downcase              ( char *txt );
-BUFFER *__buffer_new          ( int size );
-void    __buffer_strcat       ( BUFFER *buffer, const char *text );
-void    buffer_free           ( BUFFER *buffer );
-void    buffer_clear          ( BUFFER *buffer );
-int     bprintf               ( BUFFER *buffer, char *fmt, ... );
-int     mud_printf            ( char *dest, const char *format, ... );
-const char *print_header( const char *title, const char *pattern, int width );
-void bprint_commandline( void *extra, BUFFER *buf, COMMAND *com, int sublevel, int pagewidth );
-void print_commands( void *extra, LLIST *commands, BUFFER *buf, int sublevel, int pagewidth );
-char *strip_cr( const char *str );
-char *strip_nl( const char *str );
-const char *handle_pagewidth( int width, const char *txt );
-bool is_number( const char *arg );
-char *smash_color( const char *str );
-char *smash_newline( const char * str );
-int color_count( const char *str );
-void add_spaces( char *str, int amount );
-void add_lead_space( char *str, int amount );
-char *center_string( const char *to_center, int length );
-char *fit_string_to_space( const char *orig, int space );
-char *print_bar( const char *pattern, int width );
-const char *itos( int value );
-const char *quick_format( const char *Format, ... );
-const char *format_string_for_sql( const char *string );
-bool string_contains( char *string, const char *regex_string );
-
 /*
  * help.c
  */
