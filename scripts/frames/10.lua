37d75529dce0211272bb98255ea786374a8f9084
Davenge <korisd@gmail.com>
2014-12-09 16:49:06 -0500

    added .luas that will ship with the core davengine

---
+++ scripts/frames/10.lua
@@ -0,0 +1,83 @@
+function onInstanceInit( frame, instance )
+   frame:inherits( "onInstanceInit", "fi", frame:getInheritance(), instance )
+   --- autowrite init ---
+   --- end autowrite init ---
+end
+
+function onEntityEnter( room, entity )
+end
+
+function onEntityLeave( room, entity )
+end
+
+function onEntityEntering( room, entity )
+end
+
+function onEntityLeaving( room, entity )
+end
+
+function onGreetEntity( greeter, entity )
+end
+
+function onFarewellEntity( farewell, entity )
+end
+
+function onSay( entity, sayer )
+end
+
+function onGive( entity, giver )
+end
+
+function dodgeChance( attacker, dodger )
+   return 0
+end
+
+function parryChance( attacker, parryr )
+   return 0
+end
+
+function missChance( attacker, victim )
+   return 0
+end
+
+-- calculate any factors effecting melee cooldowns
+function meleeCooldown( attacker )
+   return automelee_delay
+end
+
+-- check to see if the attacker can atk the victim, ie same room? can see? etc etc --
+-- return anything other than a string if you can attack, otherwise return the reason --
+-- for example "%s is not out of range" or "%s is not here" whatever you want chummy --
+function meleeCheck( attacker, victim )
+end
+
+-- design the duration/frequency parts of the damage object
+function prepMeleeTimer( attacker, timer )
+end
+
+-- figure out the damage amount portion of the damage object
+function prepMeleeDamage( attacker, damage ) 
+end
+
+-- analyze the damage object sent to the defender
+-- calculator any the actual damage that the object does
+-- note that any elemental damage will be handled by C
+function onReceiveDamage( defender, damage )
+end
+
+-- return order attacker -> defender -> room
+function combatMessage( attacker, defender, damage, status )
+end
+
+-- returns the amount of time it takes for this corpse to decay based on the instance it was passed
+function corpseDecay( instance )
+   return standard_corpse_decay
+end
+
+-- move the items from the instance to the corpse
+-- the reason this is scripts is to handle any exceptions
+-- for example, maybe unique items don't go to your corpse
+function inventoryToCorpse( instance, corpse )
+end
+
+
